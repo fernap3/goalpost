f766508a20707000c657eb1dc9055c26c7176793
Peter Fernandes <supersonicandtails@gmail.com>
2018-08-12 19:31:39 -0400

    Add histogram of daily spending

--- account-card.js
+++ account-card.js
@@ -11,7 +11,7 @@ class AccountCardElement extends HTMLElement
 
 		console.log(`Stats for this week:`);
 		console.log(`Total spent: ${totalSpentThisWeek}`);
-		console.table(transactions);
+		
 
 		const budgetLeft = weeklyBudget - totalSpentThisWeek;
 		const percentLeft = budgetLeft / weeklyBudget;
@@ -25,6 +25,25 @@ class AccountCardElement extends HTMLElement
 		else
 			color = `var(--color-status-warning)`;
 
+		const spendingByDay = [[], [], [], [], [], [], []];
+		transactions.sort((a, b) => a.date < b.date ? -1 : a.date > b.date ? 1 : 0);
+		
+		for (let t of transactions)
+		{
+			const d = new Date(t.date);
+
+			if (t.amount > 0)
+				spendingByDay[d.getDay()].push(t);
+		}
+
+		const summedSpendingByDay = spendingByDay.map(list => list.reduce((prev, cur) => prev + cur.amount, 0));
+		const maxSpendingInOneDay = summedSpendingByDay.reduce((prev, cur) => cur > prev ? cur : prev, -1);
+
+		
+		console.table(spendingByDay);
+		console.log(summedSpendingByDay);
+		console.log("Max: ", maxSpendingInOneDay)
+
 		const template = document.createElement("template");
 		template.innerHTML = `
 			<div class="account__header">
@@ -34,10 +53,29 @@ class AccountCardElement extends HTMLElement
 			<div class="account__body" style="display:none">
 				<p>Spent this week:</p>
 				<p><span class="account__spent" style="color:${color}">$${(Math.round(totalSpentThisWeek * 100) / 100).toLocaleString()}</span> / $${weeklyBudget}</p>
+				<div class="account__body__histogram">
+					${summedSpendingByDay.map(s => `<div class="account__body__histogram__bar" style="height:${(s/maxSpendingInOneDay)*100}%;background:${s > weeklyBudget/7 ? "var(--color-status-danger)" : s / (weeklyBudget/7) > .8 ? "var(--color-status-warning)" : "var(--color-status-ok)" }"></div>`).join("")}
+					<div class="account__body__histogram__daylabel">Mon</div>
+					<div class="account__body__histogram__daylabel">Tue</div>
+					<div class="account__body__histogram__daylabel">Wed</div>
+					<div class="account__body__histogram__daylabel">Thu</div>
+					<div class="account__body__histogram__daylabel">Fri</div>
+					<div class="account__body__histogram__daylabel">Sat</div>
+					<div class="account__body__histogram__daylabel">Sun</div>
+				</div>
 			</div>
 		`;
 
 		this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+
+		const budgetLinePos = 100 - (((weeklyBudget / 7) / maxSpendingInOneDay) * 100);
+		const budgetLine = document.createElement("div");
+		budgetLine.className = "budget-line";
+		budgetLine.style.top = `${budgetLinePos}px`;
+		this.shadowRoot.querySelector(".account__body__histogram__bar").appendChild(budgetLine);
+
+
 		
 		const style = document.createElement("style");
 		style.innerHTML = `
@@ -50,10 +88,11 @@ class AccountCardElement extends HTMLElement
 				width: 100%;
 				max-width: 450px;
 				box-sizing: border-box;
+				font-size: 18px;
 			}
 
 			.account__spent {
-				font-size: 40px;
+				font-size: 2.22em;
 				font-weight: 200;
 			}
 			
@@ -69,12 +108,38 @@ class AccountCardElement extends HTMLElement
 			}
 			
 			.account__header__name {
-				font-size: 18px;
+				
 			}
 			
 			.account__header__balance {
 				margin-left: auto;
-				font-size: 17px;
+			}
+
+			.account__body__histogram {
+				display: grid;
+				grid-template-columns: repeat(7, 1fr);
+				grid-template-rows: 100px;
+				grid-column-gap: 10px;
+				position: relative;
+			}
+
+			.account__body__histogram__bar {
+				border-radius: 3px;
+				align-self: flex-end;
+			}
+
+			.account__body__histogram__daylabel {
+				justify-self: center;
+				font-size: .8em;
+			}
+
+			.budget-line {
+				width: 100%;
+				height: 2px;
+				position: absolute;
+				top: 0;
+				left: 0;
+				border-bottom: 2px dashed #222222;
 			}
 		`;
 		this.shadowRoot.appendChild(style);
